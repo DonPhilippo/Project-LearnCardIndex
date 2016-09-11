c46d9319f84091a0bf30072d9b6335cfe8b6fc24
Oliver Viete <oliver.viete@gmx.de>
2016-09-11 10:32:15 +0200

    Controller functionality added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,42 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+
 public class Controller {
+
+
+    public Controller(){
+        System.out.println("Controller funzt");
+    }
+
+
+    @FXML
+    private TextField txtLoginUsername;
+
+
+    @FXML
+    private Button btnLoginOk;
+
+    @FXML
+    private PasswordField txtLoginPassword;
+
+    @FXML
+    void loginAction(ActionEvent event) {
+        System.out.println("OK");
+    }
+
+    @FXML
+    void loginAbort(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerAction(ActionEvent event) {
+        System.out.println("Regstrieren");
+    }
+
 }
